17d18281d757f47b951b7e51de82b5b3c1ae0c54
YixianLu <yixian@ualberta.ca>
2021-02-11 09:34:43 +0800

    my first article

--- publishconf.py
+++ publishconf.py
@@ -10,13 +10,15 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = ''
+SITEURL = 'https://CMPUT404-lab5.github.io/'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
+
+OUTPUT_PATH = "docs/"
 
 # Following items are often useful when publishing
 
